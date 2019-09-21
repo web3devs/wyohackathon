b3c29ed9a677a2982d3426690fde967dfea62010
Brian Wentzloff <brian.wentzloff@gmail.com>
2019-09-21 08:28:59 -0600

    test paying dues

--- contracts/test/dfs.test.js
+++ contracts/test/dfs.test.js
@@ -2,7 +2,7 @@ const DfsContract = artifacts.require("DfsContract");
 
 contract("MetaCoin", accounts => {
     it("should deploy contract", () =>
-    DfsContract.deployed()
+      DfsContract.deployed()
         .then(balance => {
           assert.equal(
             1,
@@ -11,5 +11,24 @@ contract("MetaCoin", accounts => {
           );
         }));
   
+    it("should pay dues successfully", async () => {
+        
+        let duesAmount = 10;
+
+        let instance = await DfsContract.deployed();
+
+        returnValue = await instance.payDues({
+            from: accounts[0],
+            value: duesAmount
+        });
+
+        paidAmount = await instance.paid.call(accounts[0]);
+        assert.equal(
+            paidAmount.toNumber(),
+            duesAmount,
+            "Dues were not paid successfully"
+          );
+
+    });
     
-  });
+});
