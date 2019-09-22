3a8e84a101934459e5420184e733abbcd21d547b
Aaron Anderson <aaronlynanderson@gmail.com>
2019-09-22 08:10:16 -0600

    Updates

---
+++ precache-manifest.9e3b56171fcb11d1a04112aa3d397f8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56d9c4a49d513aa8660b3c1fc3fe8bf",
+    "url": "/wyohackathon/index.html"
+  },
+  {
+    "revision": "3b424ed9ffe349571f59",
+    "url": "/wyohackathon/static/css/2.5908778d.chunk.css"
+  },
+  {
+    "revision": "3b424ed9ffe349571f59",
+    "url": "/wyohackathon/static/js/2.845e99c0.chunk.js"
+  },
+  {
+    "revision": "6cebee12aadb1539156a",
+    "url": "/wyohackathon/static/js/main.62c10a42.chunk.js"
+  },
+  {
+    "revision": "484b8adde396d7bf65b1",
+    "url": "/wyohackathon/static/js/runtime-main.a3348bba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd2588f40b9cfe79b640adcbd0f2218e.js"
+  "/wyohackathon/precache-manifest.9e3b56171fcb11d1a04112aa3d397f8a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wyohackathon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
